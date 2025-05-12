2a17d371b33c1e5f904a67c4b37767379b9d464f
lclaglvoa <lclaglvoa@gmail.com>
2025-05-12 19:28:11 +0600

    добавил новый print в main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 print("Hello World!")
+
+print("Привет после первого коммита!")
+
+# Изначально git не знает что это папка должна 
+# иметь контроль версий
+
+# git init - 
+# git status - 
+# git add путь_к_файлу -
+# git commit -m -
+# git log -
